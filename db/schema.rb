bf3ec12921858e9c5a7e15f12ac8f8e3474e82df
vickiliu8 <vickiliu@umich.edu>
2015-07-28 23:06:42 +0200

    more styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728195044) do
+ActiveRecord::Schema.define(version: 20150728205717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150728195044) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "image"
   end
 
   add_index "photos", ["user_id"], name: "index_photos_on_user_id", using: :btree
