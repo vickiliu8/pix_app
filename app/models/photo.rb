f1737fb25839d518e6368bce8e5640c955a5a34f
vickiliu8 <vickiliu@umich.edu>
2015-07-29 11:52:35 +0200

    more styling

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,6 +7,6 @@ class Photo < ActiveRecord::Base
 	    self.save
  	end
 
- 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+ 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", dependent: :destroy
  	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
