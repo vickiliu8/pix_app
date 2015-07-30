6103f76842da8578b4de34b15c75875830a7f3bd
vickiliu8 <vickiliu@umich.edu>
2015-07-30 16:30:44 +0200

    Removed s3 from development.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,15 +1,14 @@
 class Photo < ActiveRecord::Base
 	belongs_to :users
 	has_many :comments, dependent: :destroy
+ 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", dependent: :destroy
+ 	        # :storage => :s3,
+          #   :bucket  => ENV['MY_BUCKET_NAME']
+            
+ 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
-	def liked!
+ 	def liked!
 	    self.likes = self.likes + 1
 	    self.save
  	end
-
- 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", dependent: :destroy,
- 	        :storage => :s3,
-            :bucket  => ENV['MY_BUCKET_NAME']
-            
- 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,13 +38,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.paperclip_defaults = {
-    :storage => :s3,
-    s3_host_name: 's3-website-us-east-1.amazonaws.com',
-    :s3_credentials => {
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-   },
-   :bucket => ENV['AWS_BUCKET'],
-  }
+  # config.paperclip_defaults = {
+  #   :storage => :s3,
+  #   s3_host_name: 's3-website-us-east-1.amazonaws.com',
+  #   :s3_credentials => {
+  #     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  #  },
+  #  :bucket => ENV['AWS_BUCKET'],
+  # }
 end
