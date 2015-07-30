ebe5db8ac5bfff1d982ad62093f3358899cf9b37
vickiliu8 <vickiliu@umich.edu>
2015-07-30 16:54:02 +0200

    put s3 params in Photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,9 @@
 class Photo < ActiveRecord::Base
 	belongs_to :users
 	has_many :comments, dependent: :destroy
- 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", dependent: :destroy
- 	        # :storage => :s3,
-          #   :bucket  => ENV['MY_BUCKET_NAME']
+ 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", dependent: :destroy,
+ 	        :storage => :s3,
+            :bucket  => ENV['AWS_BUCKET']
             
  	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
