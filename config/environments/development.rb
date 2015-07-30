7c108ea995f905420dfc2873cca12cc1f45fbecf
vickiliu8 <vickiliu@umich.edu>
2015-07-30 10:44:41 +0200

    tried fixing s3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,11 +40,11 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_protocol => 'https',
+    s3_host_name: 's3-website-us-east-1.amazonaws.com',
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+   },
+   :bucket => ENV['AWS_BUCKET'],
   }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,14 +77,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.paperclip_defaults = {
+  
+    config.paperclip_defaults = {
     :storage => :s3,
-    :s3_protocol => 'https',
+    s3_host_name: 's3-website-us-east-1.amazonaws.com',
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :bucket => ENV['AWS_BUCKET'],
   }
 
 end
